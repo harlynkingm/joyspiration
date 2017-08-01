1454a3a65d074feeacc53c6edfc5859b65221d1b
Max Harlynking <mharlynk@andrew.cmu.edu>
2017-08-01 11:49:26 -0400

    Reformatted blog post pictures

--- src/partials/BlogPost.js
+++ src/partials/BlogPost.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import $ from 'jquery';
 
 export default class BlogPost extends Component {
+  componentDidMount() {
+    this.setHeights();
+    window.addEventListener("resize", this.setHeights);
+  }
 
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.setHeights);
+  }
+
+  setHeights(){
+    $("figure").next("figure").each(function() {
+      //console.log($(this).prev().find("img").height());
+      //console.log($(this).find("img").height());
+      let height1 = $(this).prev().find("img").height();
+      let height2 = $(this).find("img").height();
+      let minHeight = Math.min(height1, height2);
+      $(this).prev().height(minHeight);
+      $(this).height(minHeight);
+    });
+  }
 
   render() {
     let time = moment.unix(this.props.post.postTime).format("dddd, MMMM Do");

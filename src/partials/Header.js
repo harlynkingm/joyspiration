74f94a2d605bd4078d11668b82fa56b08f14cf20
Max Harlynking <mharlynk@andrew.cmu.edu>
2017-08-03 22:06:16 -0400

    added facebook sharing

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Bitmoji from './Bitmoji';
 import insta from '../img/instagram.png';
+import share from '../img/share.png';
 import blog from '../img/blog.png';
 
 export default class Header extends Component {
@@ -32,6 +33,13 @@ export default class Header extends Component {
           </h2>
         </a>
         <div className="header-right">
+          <div className="header-icon">
+            &nbsp;
+            <a href="#" onClick={function(){ window.open('https://www.facebook.com/sharer/sharer.php?u=http%3A//joyspirationblog.com/','facebook-share-dialog','width=626,height=436');return false;}} href="javascript:void(0)">
+              <img src={share} alt="Share on Facebook" className="share"/>
+            </a>
+            <span className="subtitle unselectable">Share</span>
+          </div>
           <div className="header-icon">
             &nbsp;
             <a href="https://www.instagram.com/joyspirationblog/" target="_blank" rel="noopener noreferrer">
